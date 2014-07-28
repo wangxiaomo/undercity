22eba5c7a28e5689f1a42ab8a0b9ce70cb18dd38
wangxiaomo <wxm4ever@gmail.com>
2014-07-28 10:54:06 +0800

    update local.pp

--- dev/puppet/manifests/nodes/default.pp
+++ dev/puppet/manifests/nodes/default.pp
@@ -1,4 +1,16 @@
 node default {
     include common
     include sitemap::disable
+
+    package { ["samba", "samba-client", "samba-swat"]:
+        ensure  => installed,
+    }
+    
+    service { "smb":
+        ensure    => running,
+        enable    => true,
+        hasstatus => true,
+        hasrestart=> true,
+        require   => Package["samba"]
+    }
 }
